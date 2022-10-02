280a64ca387107612da6e782b0acd3cd16ceab10
小企鹅 <2557173038@qq.com>
2022-10-02 18:01:10 +0800

    maizuo commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,32 @@
 <template>
   <div>
     <van-form @submit="onSubmit">
-  <van-field
-    v-model="username"
-    name="用户名"
-    label="用户名"
-    placeholder="用户名"
-    :rules="[{  message: '请填写用户名' }]"
-  />
-  <van-field
-    v-model="password"
-    type="password"
-    name="密码"
-    label="密码"
-    placeholder="无需填写"
-    :rules="[{ message: '请填写密码' }]"
-  />
-  <div style="margin: 16px;">
-    <van-button round block type="info" native-type="submit" @click="handleLogin">登录</van-button>
-  </div>
-</van-form>
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="无需填写"
+        :rules="[{ message: '请填写密码' }]"
+      />
+      <div style="margin: 16px">
+        <van-button
+          round
+          block
+          type="info"
+          native-type="submit"
+          @click="handleLogin"
+          >登录</van-button
+        >
+      </div>
+    </van-form>
   </div>
 </template>
 
@@ -27,8 +34,8 @@
 export default {
   data() {
     return {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     };
   },
   methods: {
